9c74f43927b492553ec42fc11920231e6f23f3d8
Imran Gilani <imran.gilani@bloomingdales.com>
2018-11-13 17:51:22 -0500

    Adding dynamic classes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,9 +2,11 @@
   <div class="homepage">
    ================================ <br/>   
    <h3>{{ title }}</h3>
-   <div class="demo"></div>
-   <div class="demo"></div>
-   <div class="demo"></div>
+   <div class="demo" @click="attachRed = !attachRed" :class="{red:attachRed}">
+   </div>
+   <div class="demo" :class="divClasses"></div>
+   <div class="demo" :class="color"></div>
+   <input type="text" v-model="color">
   </div>
 </template>
 
@@ -13,11 +15,21 @@ export default {
   name: "HomePage",
   data: function() {
     return {
-      title: "Welcome to the home-portal Imran!"
+      title: "Welcome to the home-portal Imran!",
+      attachRed: false,
+      attachGreen: false,
+      color: "green"
     };
   },
   methods: {},
-  computed: {},
+  computed: {
+    divClasses: function() {
+      return {
+        red: this.attachRed,
+        blue: !this.attachRed
+      };
+    }
+  },
   watch: {}
 };
 </script>
@@ -26,5 +38,21 @@ export default {
 <style scoped>
 .homepage h3 {
   color: blue;
+}
+.demo {
+  width: 100px;
+  height: 100px;
+  background-color: grey;
+  display: inline-block;
+  margin: 10px;
+}
+.red {
+  background-color: red;
+}
+.green {
+  background-color: green;
+}
+.blue {
+  background-color: blue;
 }
 </style>
