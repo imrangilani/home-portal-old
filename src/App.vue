8e8d6818b15587919cbb54d8c3971589f2fb4b4a
Imran Gilani <imrangilani@Imrans-MacBook-Pro.local>
2019-02-11 21:26:55 -0500

    Creating dynamic component new app realign

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,24 @@
   <div id="appContainer">
     <img alt="Vue logo" src="./assets/logo.png"><br>
     <button @click="selectedComponent= 'Container'">Show Container</button>
-    <button @click="selectedComponent= 'QuoteGrid'">Show QuoteGrid</button>
+    <button @click="selectedComponent= 'QuoteApp'">Show QuoteApp</button>
     <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import Container from "./components/Container.vue"
-import QuoteGrid from "./components/QuoteGrid.vue"
+import QuoteApp from "./components/QuoteApp.vue"
 
 export default {
   name: "mainApp",
   components: {
     Container,
-    QuoteGrid
+    QuoteApp
   },
   data: function() {
     return {
-      selectedComponent: QuoteGrid
+      selectedComponent: QuoteApp
     };
   },
   methods: {
